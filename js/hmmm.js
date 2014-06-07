b1dbef800492d6a553cb35501cd7739ecef973b7
Julia Bode <julia.bode@hmmh.de>
2014-06-07 15:53:49 +0200

    Add basic autocomplete search.

--- js/hmmm.js
+++ js/hmmm.js
@@ -2,4 +2,16 @@ window.onload = function () {
     'use strict';
     $(document).foundation();
 
+    $(function() {
+    var availableContacts = [
+        'Julia Bode',
+        'Tatjana Kiel',
+        'Sophie Vo',
+        'Eva Nesbach',
+        'Daniel Opitz'
+    ];
+    $( "#multi-search-input" ).autocomplete({
+      source: availableContacts
+    });
+  });
 };
