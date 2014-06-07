6f0e8c334d6b5afdd852ef538796cb354a5185fd
Julia Bode <julia.bode@hmmh.de>
2014-06-08 01:00:48 +0200

    Add styling to profile overview.

--- js/hmmm.js
+++ js/hmmm.js
@@ -3,15 +3,46 @@ window.onload = function () {
     $(document).foundation();
 
     $(function() {
-    var availableContacts = [
-        'Julia Bode',
-        'Tatjana Kiel',
-        'Sophie Vo',
-        'Eva Nesbach',
-        'Daniel Opitz'
+    var availableContacts = [{
+            label: 'Kitty Kitten',
+            icon: 'fa-user'
+        },
+        {
+            label: 'Julia Bode',
+            icon: 'fa-user'
+        },
+        {
+            label: 'Tatjana Kiel',
+            icon: 'fa-user'
+        },
+        {
+            label: 'Sophie Vo',
+            icon: 'fa-user'
+        },
+        {
+            label: 'Eva Nesbach',
+            icon: 'fa-user'
+        },
+        {
+            label: 'Daniel Opitz',
+            icon: 'fa-user'
+        },
+        {
+            label: 'Favorite Animal',
+            icon: 'fa-comment-o'
+        }
     ];
     $( "#multi-search-input" ).autocomplete({
-      source: availableContacts
-    });
+        source: availableContacts,
+        select: function (event, ui) {
+             $('#multi-search-input').val(ui.item.label);
+             return false;
+        }
+    }).data("ui-autocomplete")._renderItem = function (ul, item) {
+         return $("<li></li>")
+             .data("item.autocomplete", item)
+             .append("<li><a href='./profile-overview.html'>" + item.label + "<i class='fa " + item.icon + " right'></i></a></li>")
+             .appendTo(ul);
+     };
   });
 };
